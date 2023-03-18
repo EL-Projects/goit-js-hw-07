f8afcf3175cbcda6bfe3fa7eb58f74b668f36877
Your Name <you@example.com>
2023-03-18 14:57:54 +0200

    createdlightbox js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,19 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = galleryItems;
+const ulGallery = document.querySelector("ul.gallery");
+const markup = gallery
+  .map(({ preview, original, description }) => {
+    return `<a class="galery__item" href="${original}">
+<img class="gallery__image" src="${preview}" alt="${description}"/>
+    </a>`;
+  })
+  .join("");
+ulGallery.insertAdjacentHTML("beforeend", markup);
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: "250",
+});
+lightbox.on("show.SimpleLightbox");
